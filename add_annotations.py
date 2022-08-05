f68d051e48003abebdcffbb4509b13d30e49798b
sradkani <sradkani@gmail.com>
2022-08-05 10:53:47 -0700

    fixing the annotations

--- add_annotations.py
+++ add_annotations.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def add_coco_annot(response_df):
     info_df = pd.read_csv('stim_info_coco_annot.csv')
@@ -15,9 +16,25 @@ def add_coco_annot(response_df):
 def add_manual_annot(response_df):
     motion_annotations = pd.read_csv('annot_app/motion_annotations.csv')
     interaction_annotations = pd.read_csv('annot_app/interaction_annotations.csv')
-    tmp =  pd.merge(response_df, interaction_annotations, on="73KID", how="outer")
-    tmp =  pd.merge(tmp, motion_annotations, on="73KID", how="outer")
-    return tmp
+
+    interaction_type = []
+    motion_level = []
+    interaction_IDs = interaction_annotations['73KID'].tolist()
+    motion_IDs = motion_annotations['73KID'].tolist()
+    for index, row in response_df.iterrows():
+        if row['73KID'] in interaction_IDs:
+            interaction_type.append(interaction_annotations[interaction_annotations['73KID'] == row['73KID']]['interaction_type'].values[0])
+        else:
+            interaction_type.append(np.nan)
+        if row['73KID'] in motion_IDs:
+            motion_level.append(motion_annotations[motion_annotations['73KID'] == row['73KID']]['motion_level'].values[0])
+        else:
+            motion_level.append(np.nan)
+    response_df['interaction_type'] = interaction_type
+    response_df['motion_level'] = motion_level
+    # tmp =  pd.merge(response_df, interaction_annotations, on="73KID", how="outer")
+    # tmp =  pd.merge(tmp, motion_annotations, on="73KID", how="outer")
+    return response_df
 
 def add_shared1000(response_df):
     info_df = pd.read_csv('stim_info_coco_annot.csv')
@@ -41,7 +58,7 @@ def main():
     ## Add the manual annotations
     final_response_df = add_manual_annot(coco_annotated_shared1000_response_df)
 
-    ## Save the final fully annotated and processed response file
+    # Save the final fully annotated and processed response file
     final_response_df.to_csv('subj01/behav/final_response.tsv', sep='\t', index=False)
 
 if __name__=="__main__":
